ba8c86d7806f95f5fc7fb0c91a417379befa7e07
Adam Griffin <adamgriffin@Adams-MBP.lan>
2020-10-26 23:47:44 -0700

    Added pop-up menu for event creation.

--- web/src/view/page/components/calendar.tsx
+++ web/src/view/page/components/calendar.tsx
@@ -1,14 +1,16 @@
-import { AppointmentModel } from '@devexpress/dx-react-scheduler';
+import { AppointmentModel, ChangeSet, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler'
 import {
+  AppointmentForm,
   Appointments,
   AppointmentTooltip,
+  ConfirmationDialog,
   Resources,
   Scheduler,
   WeekView
-} from '@devexpress/dx-react-scheduler-material-ui';
-import Paper from '@material-ui/core/Paper';
+} from '@devexpress/dx-react-scheduler-material-ui'
+import Paper from '@material-ui/core/Paper'
 // import { RouteComponentProps } from '@reach/router'
-import * as React from 'react';
+import * as React from 'react'
 
 // interface HomePageProps extends RouteComponentProps, AppRouteParams {}
 
@@ -23,8 +25,7 @@ const resources = [
   },
 ]
 
-const Appointment: React.ComponentType<Appointments.AppointmentProps> = (props) =>
-{
+const Appointment: React.ComponentType<Appointments.AppointmentProps> = props => {
   return <Appointments.Appointment {...props} onClick={() => window.open(props.data.zoom)} />
 }
 
@@ -35,7 +36,7 @@ const appointments: Array<AppointmentModel> = [
     title: 'CS 118',
     type: 'class',
     rRule: 'FREQ=WEEKLY;BYDAY=TU,TH',
-    zoom: 'https://ucla.zoom.us/j/95034758361?pwd=WWtFYU9YVWpLTlVMeCt0RFBuTEg1Zz09'
+    zoom: 'https://ucla.zoom.us/j/95034758361?pwd=WWtFYU9YVWpLTlVMeCt0RFBuTEg1Zz09',
   },
   {
     startDate: new Date(2020, 9, 12, 8, 0), // Month is 0-indexed
@@ -43,21 +44,58 @@ const appointments: Array<AppointmentModel> = [
     title: 'CS 188',
     type: 'class',
     rRule: 'FREQ=WEEKLY;BYDAY=MO,WE',
-    zoom: 'https://ucla.zoom.us/j/92470409406?pwd=eFpyYWFQZGRtcVUzWC9HYlhSakRxZz09'
+    zoom: 'https://ucla.zoom.us/j/92470409406?pwd=eFpyYWFQZGRtcVUzWC9HYlhSakRxZz09',
   },
 ]
 
-export function Calendar() {
-  return (
-    <React.Fragment>
-      <Paper>
-        <Scheduler height={600} data={appointments}>
-          <WeekView startDayHour={8} endDayHour={20} cellDuration={60} />
-          <Appointments appointmentComponent={Appointment}/>
-          <AppointmentTooltip visible={false}/>
-          <Resources data={resources} />
-        </Scheduler>
-      </Paper>
-    </React.Fragment>
-  )
+export class Calendar extends React.PureComponent<any, any> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      data: appointments,
+    }
+
+    this.commitChanges = this.commitChanges.bind(this)
+  }
+
+  commitChanges(changes: ChangeSet) {
+    const { added, changed, deleted } = changes
+    this.setState((state: any) => {
+      let { data } = state
+      if (added) {
+        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0
+        data = [...data, { id: startingAddedId, ...added }]
+      }
+      if (changed) {
+        data = data.map((appointment: any) =>
+          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment
+        )
+      }
+      if (deleted !== undefined) {
+        data = data.filter((appointment: any) => appointment.id !== deleted)
+      }
+      return { data }
+    })
+  }
+
+  render() {
+    const { data } = this.state
+
+    return (
+      <React.Fragment>
+        <Paper>
+          <Scheduler height={600} data={data}>
+            <WeekView startDayHour={8} endDayHour={20} cellDuration={60} />
+            <EditingState onCommitChanges={this.commitChanges} />
+            <Appointments appointmentComponent={Appointment} />
+            <IntegratedEditing />
+            <ConfirmationDialog />
+            <Resources data={resources} />
+            <AppointmentTooltip visible={false} />
+            <AppointmentForm />
+          </Scheduler>
+        </Paper>
+      </React.Fragment>
+    )
+  }
 }
